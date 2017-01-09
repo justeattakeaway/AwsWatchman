af6e1db479c40bfda5b1af7b74a1ee1096d08430
Anthony Steele <anthony.steele@just-eat.com>
2017-01-09 17:24:54 +0000

    Le tests

---
+++ Watchman.Engine.Tests/AlarmDefinitionTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Watchman.Engine.Tests
+{
+    [TestFixture]
+    public class AlarmDefinitionTests
+    {
+        [Test]
+        public void TestDefaultFlagsState()
+        {
+            var alarmDef = new AlarmDefinition();
+
+            Assert.That(alarmDef.AlertOnInsufficientData, Is.False);
+            Assert.That(alarmDef.AlertOnOk, Is.True);
+        }
+
+        [Test]
+        public void TestFlagsCopy()
+        {
+            var original = new AlarmDefinition
+                {
+                    AlertOnInsufficientData = true,
+                    AlertOnOk = false
+                };
+
+            var copy = original.Copy();
+
+            Assert.That(copy.AlertOnInsufficientData, Is.True);
+            Assert.That(copy.AlertOnOk, Is.False);
+        }
+    }
+}
