62eae4e634a8341fa70b30c247875b3d856b222a
Anthony Steele <Anthony@PlasticAvalon.org>
2017-02-03 12:28:17 +0000

    Test alert target equality code (#32)
    * Test alert target equality code
    
    * Stronger tests
    Fail because hashes collide across types
    Probably not a real issue because urls and emails never have the same text as each other
    
    * test on the getHashCode edge case

---
+++ Watchman.Configuration.Tests/AlertTargetTests.cs
@@ -0,0 +1,124 @@
+﻿using NUnit.Framework;
+
+namespace Watchman.Configuration.Tests
+{
+    [TestFixture]
+    public class AlertTargetTests
+    {
+        [Test]
+        public void EqualsIsFalseForNullEmail()
+        {
+            var instance = new AlertEmail { Email = "a@b.com" };
+
+            Assert.That(instance, Is.Not.EqualTo(null));
+        }
+
+        [Test]
+        public void EqualsIsFalseForNullUrl()
+        {
+            var instance = new AlertUrl { Url = "http://a.com/b" };
+
+            Assert.That(instance, Is.Not.EqualTo(null));
+        }
+
+        [Test]
+        public void SelfEqualIsTrue()
+        {
+            var instance1 = new AlertUrl { Url = "http://a.com/b" };
+            var instance2 = new AlertEmail { Email = "abc" };
+
+            Assert.That(instance1, Is.EqualTo(instance1));
+            Assert.That(instance2, Is.EqualTo(instance2));
+
+            Assert.That(instance1, Is.Not.EqualTo(instance2));
+            Assert.That(instance2, Is.Not.EqualTo(instance1));
+        }
+
+        [Test]
+        public void DifferentEmailAndUrlAreNotEqual()
+        {
+            var instance1 = new AlertUrl { Url = "a@b.c" };
+            var instance2 = new AlertEmail { Email = "http://abc" };
+
+            AssertInequalities(instance1, instance2);
+        }
+
+        [Test]
+        public void SameEmailAndUrlAreNotEqual()
+        {
+            var instance1 = new AlertUrl { Url = "a.b.c" };
+            var instance2 = new AlertEmail { Email = "a.b.c" };
+
+            // Can't call AssertInequalities because the hashcodes are same for same strings
+            // This is ot a problem in practice
+            // as emails and urls have different formats
+
+            Assert.That(instance1, Is.Not.EqualTo(instance2));
+            Assert.That(instance2, Is.Not.EqualTo(instance1));
+        }
+
+        [Test]
+        public void EqualsIsTrueForEquivalentEmails()
+        {
+            var instance1 = new AlertEmail { Email = "a@b.com" };
+            var instance2 = new AlertEmail { Email = "a@b.com" };
+
+            AssertEqualities(instance1, instance2);
+        }
+
+        [Test]
+        public void EqualsIsFalseForDifferentEmails()
+        {
+            var instance1 = new AlertEmail { Email = "a@b.com" };
+            var instance2 = new AlertEmail { Email = "beee@ceee.org" };
+
+            AssertInequalities(instance1, instance2);
+        }
+
+        [Test]
+        public void EqualsIsTrueForEquivalentUrls()
+        {
+            var instance1 = new AlertUrl { Url = "http://a.com/b" };
+            var instance2 = new AlertUrl { Url = "http://a.com/b" };
+
+            AssertEqualities(instance1, instance2);
+        }
+
+        [Test]
+        public void EqualsIsFalseForDifferentUrls()
+        {
+            var instance1 = new AlertUrl { Url = "http://a.com/b" };
+            var instance2 = new AlertUrl { Url = "https://beee.org/ceee" };
+
+            AssertInequalities(instance1, instance2);
+        }
+
+        private void AssertEqualities(AlertTarget a, AlertTarget b)
+        {
+            Assert.That(a, Is.EqualTo(a));
+            Assert.That(b, Is.EqualTo(b));
+
+            Assert.That(a, Is.EqualTo(b));
+            Assert.That(b, Is.EqualTo(a));
+
+            Assert.That(a, Is.Not.EqualTo(null));
+            Assert.That(b, Is.Not.EqualTo(null));
+
+            Assert.That(a.GetHashCode(), Is.EqualTo(b.GetHashCode()));
+        }
+
+        private void AssertInequalities(AlertTarget a, AlertTarget b)
+        {
+            Assert.That(a, Is.EqualTo(a));
+            Assert.That(b, Is.EqualTo(b));
+
+            Assert.That(a, Is.Not.EqualTo(b));
+            Assert.That(b, Is.Not.EqualTo(a));
+
+            Assert.That(a, Is.Not.EqualTo(null));
+            Assert.That(b, Is.Not.EqualTo(null));
+
+            Assert.That(a.GetHashCode(), Is.Not.EqualTo(b.GetHashCode()));
+        }
+    }
+}
