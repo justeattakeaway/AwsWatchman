af672d12661cb3a2f4bbae71a9b7a077f16d25bb
Anthony Steele <Anthony@PlasticAvalon.org>
2017-02-03 10:27:32 +0000

    Review, refactor cloudwatch generation (#31)
    * Formatting
    
    * Stack has description

--- Watchman.Configuration/AlertUrl.cs
+++ Watchman.Configuration/AlertUrl.cs
@@ -9,15 +9,24 @@
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
             return Equals((AlertUrl) obj);
         }
 
         public override int GetHashCode()
         {
-            return Url != null ? Url.GetHashCode() : 0;
+            return Url?.GetHashCode() ?? 0;
         }
 
         public string Url { get; set; }
