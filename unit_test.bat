0b7f1f9ff955c514efc76e6b29454c72c02f1688
Anthony Steele <AnthonySteele@users.noreply.github.com>
2017-12-11 14:39:49 +0000

    Added test scripts (#79)
    simple .bat files
    for use on TC build agents

---
+++ unit_test.bat
@@ -0,0 +1,15 @@
+cd .\Quartermaster.Tests
+dotnet test
+cd ..
+
+cd .\Watchman.AwsResources.Tests
+dotnet test
+cd ..
+
+cd .\Watchman.Configuration.Tests
+dotnet test
+cd ..
+
+cd .\Watchman.Engine.Tests
+dotnet test
+cd ..
