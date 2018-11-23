8b20277409df0940544528ac083076e79409bd38
Anthony Steele <Anthony@PlasticAvalon.org>
2018-11-23 14:24:52 +0000

    Update install script

--- dotnetfunctions.ps1
+++ dotnetfunctions.ps1
@@ -50,16 +50,13 @@ if ($installDotNetSdk -eq $true) {
     $env:DOTNET_INSTALL_DIR = Join-Path "$(Convert-Path "$PSScriptRoot")" ".dotnetcli"
     $env:MSBuildSDKsPath = Join-Path $env:DOTNET_INSTALL_DIR "sdk\$dotnetVersion\Sdks"
 
-    if (($null -ne $env:TEAMCITY_VERSION) -or (!(Test-Path $sdkPath))) {
-        if (!(Test-Path $env:DOTNET_INSTALL_DIR)) {
-            mkdir $env:DOTNET_INSTALL_DIR | Out-Null
-        }
+    if (($env:TEAMCITY_VERSION -ne $null) -or (!(Test-Path $env:DOTNET_INSTALL_DIR))) {
+        mkdir $env:DOTNET_INSTALL_DIR -Force | Out-Null
         $installScript = Join-Path $env:DOTNET_INSTALL_DIR "install.ps1"
         [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
         Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile $installScript -UseBasicParsing
         & $installScript -Version "$dotnetVersion" -InstallDir "$env:DOTNET_INSTALL_DIR" -NoPath
     }
-
 
     $env:PATH = "$env:DOTNET_INSTALL_DIR;$env:PATH"
     $dotnet = Join-Path "$env:DOTNET_INSTALL_DIR" "dotnet.exe"
