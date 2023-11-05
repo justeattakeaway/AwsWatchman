b4968a3c8c38aea06b317d4edc66efd6826fa58e
Martin Costello <martin@martincostello.com>
2023-11-05 16:32:46 +0000

    Use terminal logger Use the new MSBuild terminal logger.

--- build.ps1
+++ build.ps1
@@ -88,7 +88,7 @@ function DotNetPack {
         $additionalArgs += $VersionSuffix
     }
 
-    & $dotnet pack $Project --output $PackageOutputPath --configuration $Configuration --include-symbols --include-source $additionalArgs
+    & $dotnet pack $Project --output $PackageOutputPath --configuration $Configuration --include-symbols --include-source --tl $additionalArgs
 
     if ($LASTEXITCODE -ne 0) {
         throw "dotnet pack failed with exit code $LASTEXITCODE"
@@ -105,7 +105,7 @@ function DotNetTest {
         $additionalArgs += "GitHubActions;report-warnings=false"
     }
 
-    & $dotnet test $Project --output $OutputPath --configuration $Configuration $additionalArgs
+    & $dotnet test $Project --output $OutputPath --configuration $Configuration --tl $additionalArgs
 
     if ($LASTEXITCODE -ne 0) {
         throw "dotnet test failed with exit code $LASTEXITCODE"
